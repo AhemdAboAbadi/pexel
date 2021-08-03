393deb8b2ef6bcab6b8362ad345a064ff6964aaf
aaamra <dev.aaamra@gmail.com>
2021-08-03 17:26:40 +0300

    update the router in the server.js file

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
 const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const router = require('./router.js');
 require('env2')('.env')
 
-const router = (request , response)=> {
-  response.end()
-}
-
-const port = process.env.PORT || 8080 ;
-console.log(port);
+const port = process.env.PORT || 8080;
 
 const server = http.createServer(router)
-server.listen(port,()=>{
-  console.log(`server is running at : http://localhost:${port}`);
+
+server.listen(port, () => {
+    console.log(`server is running at : http://localhost:${port}`);
 })
